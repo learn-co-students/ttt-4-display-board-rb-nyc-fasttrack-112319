db8e8103db7375e632bff4e7f31ec906dcc4c8fa
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-11-25 16:45:37 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = ["   x    |   x   |   x   ", "-----------", "      |       |      ", "-----------", ["   o    |   o   |   o   "]
+def display_board(board)
+ print "#{board[0]}"
+ print "#{board[1]}}"
+ print "#{board[2]}"
+ print "#{board[3]}"
+ print "#{board[4]}"
+ print "#{board[5]}"
+end
